790f9ed0c639c5fd533ac8b3578b93e445c9c627
ishan-spr <ishan.chaudhary@sprinklr.com>
2022-08-22 15:56:01 +0530

    Footer Done

--- src/App.js
+++ src/App.js
@@ -2,13 +2,15 @@ import './App.css';
 import Header from './components/Header';
 import HeroComponent from './components/HeroComponent';
 import InformationGrid from './components/InformationGrid';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <>
       <Header />
       <HeroComponent />
-      <InformationGrid/>
+      <InformationGrid />
+      <Footer />
     </>
   );
 }
